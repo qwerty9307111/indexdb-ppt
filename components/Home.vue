659f2aee79ab2285fc3a9377706eb4575cc1c7da
Sean.Yin <2436415068@qq.com>
2022-03-19 17:48:35 +0800

    feat: first page

---
+++ components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main absolute h-full w-full bg-black opacity-55">
+    <div class="mb-4 absolute bottom-4 left-12 opacity-100">
+      <span
+        class="text-6xl text-primary-lighter text-opacity-90"
+        style="font-weight: 500"
+      >
+        <a href="https://www.w3.org/TR/IndexedDB/" target="_blank">IndexedDB</a>
+        <light-icon icon="bike" />
+      </span>
+      <div class="text-9xl text-white text-opacity-90" style="font-weight: 600">
+        Transactional
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.slidev-page-1 {
+  color: white;
+  background-image: url(http://source.unsplash.com/collection/94734566/1920x1080);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  padding: 0 !important;
+}
+.main {
+  text-align: left;
+}
+</style>
